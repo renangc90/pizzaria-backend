dd257b231f863eb42201b1f615b6339aa9b36f42
renangc90 <renangc90@gmail.com>
2022-07-18 08:03:58 -0300

    code

---
+++ src/middlewares/isAuthenticated.ts
@@ -0,0 +1,38 @@
+import { NextFunction, request, Request, response, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface Payload {
+    sub: string
+}
+
+export function isAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+
+    //receber o token
+    const authToken = req.headers.authorization
+
+    if (!authToken) {
+        return res.status(401).end()
+    }
+
+    //pegar o token apos o bearer
+    const [, token] = authToken.split(" ")
+
+    try {
+        //validar esse token
+        const { sub } = verify(
+            token,
+            process.env.JWT_SECRET
+        ) as Payload
+        
+        //recuperar o id do token e colocar dentro de uma variavel user_id dentro do req
+        req.user_id = sub
+
+        return next()
+    } catch (error) {
+        return res.status(401).end()
+    }
+}
---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,53 @@
+import prismaClient from "../../prisma"
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+
+interface AuthRequest{
+    email: string;
+    password: string;
+}
+
+class AuthUserService{
+    async execute({ email, password }: AuthRequest){
+        //verificar se o email existe
+        const user = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+
+        })
+
+        if(!user){
+            throw new Error("User/password incorrect")
+        }
+
+        //verificar se a senha esta correta
+        const passwordMatch = await compare(password, user.password)
+
+        if(!passwordMatch){
+            throw new Error("User/password incorrect")
+        }
+
+        //gerar um jwt e devolver os dados do usuario
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
+    }
+}
+
+export { AuthUserService }
